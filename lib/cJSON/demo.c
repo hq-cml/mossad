c2d559207d5f439d1b23249dc7c46f9327ba487c
hq <hq_cml@163.com>
2015-02-03 20:23:51 +0800

    commit

--- lib/cJSON/demo.c
+++ lib/cJSON/demo.c
@@ -4,6 +4,24 @@
 
 int json_decode()
 {
+    int i;
+    cJSON *p_root;
+    cJSON *p_item1;
+    cJSON *p_item2;
+    cJSON *p_item3;
+    cJSON *p_array;
+
+    char *p_item_id;
+    char *p_value;
+    char *content_buff;
+    
+    content_buff = calloc(1, 4096);
+    strcpy(content_buff, str);
+
+    /* 苦逼的json解析，只需要cJSON_Parse的返回对象需要释放，内部会循环释放 */
+    p_root = cJSON_Parse(content_buff+10);
+    if(!p_root) goto json_null;
+    
     return 0;
 }
 
