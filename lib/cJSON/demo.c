4de365d2a94e2bce028d4ff833e8c8ab314a021c
hq <hq_cml@163.com>
2015-02-03 21:43:53 +0800

    recurse decode

--- lib/cJSON/demo.c
+++ lib/cJSON/demo.c
@@ -53,24 +53,31 @@ int recursive_parse(cJSON *p_root, char *prefix)
     }
     return 0;
 }
-    cJSON *p_root;
-    cJSON *p_item1;
-    cJSON *p_item2;
-    cJSON *p_item3;
-    cJSON *p_array;
 
-    char *p_item_id;
-    char *p_value;
+int json_decode(const char *str)
+{
+    cJSON *p_root;
     char *content_buff;
+    char pre[100] = {0};
     
     content_buff = calloc(1, 4096);
     strcpy(content_buff, str);
 
     /* 苦逼的json解析，只需要cJSON_Parse的返回对象需要释放，内部会循环释放 */
-    p_root = cJSON_Parse(content_buff+10);
+    p_root = cJSON_Parse(content_buff);
     if(!p_root) goto json_null;
     
+    recursive_parse(p_root, pre);
+    free(content_buff);
+    cJSON_Delete(p_root);
     return 0;
+    
+json_null:
+    printf("Something is wrong!\n");
+    free(content_buff);
+    cJSON_Delete(p_root);
+    return -1;
+
 }
 
 char* json_encode()
