6b9319a774d12f3a9d073ec023607fdfebf9ae14
hq <hq_cml@163.com>
2015-02-04 11:31:29 +0800

    json encode

--- lib/cJSON/demo.c
+++ lib/cJSON/demo.c
@@ -120,8 +120,67 @@ json_null:
 
 char* json_encode()
 {
+    cJSON *p_root;
+    cJSON *p_data_arr;
+    cJSON *p_data;
+    cJSON *p_value;
+    char  *result,*json;
+    json = calloc(1, 4096);
 
-    return 0; 
+    /* 拼装JSON计算结果 */
+    //{"type":"0","abc":888.888000,"data":[["abc","efg","789.111"],[123,456,789]],"arr0":["xyz",888],"arr1":{"a":"uuu","b":111}}
+    p_root = cJSON_CreateObject();
+    
+    //新增普通string和number
+    cJSON_AddStringToObject(p_root, "type", "0");
+    cJSON_AddNumberToObject(p_root, "abc",  888.888);
+    
+    //增加多为数组
+    p_data_arr = cJSON_CreateArray();
+    p_data = cJSON_CreateArray();
+    p_value = cJSON_CreateString("abc");
+    cJSON_AddItemToArray(p_data, p_value);
+    p_value = cJSON_CreateString("efg");
+    cJSON_AddItemToArray(p_data, p_value);
+    p_value = cJSON_CreateString("789.111");
+    cJSON_AddItemToArray(p_data, p_value);
+    cJSON_AddItemToArray(p_data_arr, p_data); 
+    
+    p_data  = cJSON_CreateArray();
+    p_value = cJSON_CreateNumber(123.0);
+    cJSON_AddItemToArray(p_data, p_value);
+    p_value = cJSON_CreateNumber(456.0);
+    cJSON_AddItemToArray(p_data, p_value);
+    p_value = cJSON_CreateNumber(789.0);
+    cJSON_AddItemToArray(p_data, p_value);
+    cJSON_AddItemToArray(p_data_arr, p_data); 
+    
+    cJSON_AddItemToObject(p_root, "data", p_data_arr);
+    
+    //增加一维数组
+    p_data_arr = cJSON_CreateArray();
+    p_value = cJSON_CreateString("xyz");
+    cJSON_AddItemToArray(p_data_arr, p_value);
+    p_value = cJSON_CreateNumber(888);
+    cJSON_AddItemToArray(p_data_arr, p_value);
+    cJSON_AddItemToObject(p_root, "arr0", p_data_arr); 
+
+    //增加hash
+    p_data_arr = cJSON_CreateObject();
+    cJSON_AddStringToObject(p_data_arr, "a", "uuu");
+    cJSON_AddNumberToObject(p_data_arr, "b", 111);
+    cJSON_AddItemToObject(p_root, "arr1", p_data_arr);     
+    
+    //生成格式化字符串
+    result = cJSON_PrintUnformatted(p_root);
+    
+    printf("orgin string: %s\n", result);
+    strcpy(json, result);
+    free(result);
+    
+    /* 递归删除，中间无需单独删除，否则反而会出现二次释放 */
+    cJSON_Delete(p_root);
+    return json; 
 }
 
 int main()
