2f0561f180311473ab8bb7185f0a78f3cc17a264
hq <hq_cml@163.com>
2015-02-13 18:53:43 +0800

    connect

--- demos/redis-saver/redis_saver.c
+++ demos/redis-saver/redis_saver.c
@@ -16,3 +16,43 @@
  **/
 
 #include "redis_saver.h"
+/**
+ * 功能: 单个线程初始化回调
+ * 参数: @worker
+ * 说明: 
+ *       1. 可选函数
+ * 返回:成功:0; 失败:-x
+ **/
+int msd_handle_worker_init(void *conf, void *arg)
+{
+    MSD_INFO_LOG("Msd_handle_worker_init is called!");
+    msd_thread_worker_t *worker = (msd_thread_worker_t *)arg;
+
+    saver_worker_data_t *worker_data;
+    
+    if(!(worker->priv_data = calloc(1, sizeof(saver_worker_data_t))))
+    {
+        MSD_ERROR_LOG("Calloc priv_data failed!");
+        return MSD_ERR;
+    }
+    worker_data = worker->priv_data;
+    if(!(worker_data->redis_ip= msd_str_new(msd_conf_get_str_value(conf,"redis_ip",NULL))))
+    {
+        MSD_ERROR_LOG("Get redis_ip Failed!");
+        return MSD_ERR;
+    }
+    if(!(worker_data->redis_port= msd_str_new(msd_conf_get_str_value(conf,"redis_port",NULL))))
+    {
+        MSD_ERROR_LOG("Get redis_port Failed!");
+        return MSD_ERR;
+    }
+    if(!(worker_data->redis_db= msd_str_new(msd_conf_get_str_value(conf,"redis_db",NULL))))
+    {
+        MSD_ERROR_LOG("Get redis_db Failed!");
+        return MSD_ERR;
+    }
+    
+    worker_data->worker = worker;
+ 
+    return MSD_OK;
+}
