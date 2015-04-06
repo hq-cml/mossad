fcb0207a63d23cee3688303811b02853ac530bbb
hq <hq_cml@163.com>
2015-04-06 15:48:27 +0800

    hash clear

--- inc/msd_hash.h
+++ inc/msd_hash.h
@@ -113,6 +113,7 @@ int msd_hash_foreach(msd_hash_t *ht,
         void *userptr);
 void *msd_hash_get_val(msd_hash_t *ht, const void *key);
 void msd_hash_destroy(msd_hash_t *ht);
+void msd_hash_clear(msd_hash_t *ht);
 void msd_hash_free(msd_hash_t *ht);
 int msd_hash_delete_foreach(const msd_hash_entry_t *he, void *ht);
 msd_hash_t *msd_hash_duplicate(msd_hash_t *ht);
