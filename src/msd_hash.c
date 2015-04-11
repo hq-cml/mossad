8802b10777b0b9a714d323a4c7d7f649c65e681a
hq <hq_cml@163.com>
2015-04-11 11:17:43 +0800

    comment

--- src/msd_hash.c
+++ src/msd_hash.c
@@ -365,7 +365,7 @@ static int msd_hash_delete_foreach(const msd_hash_entry_t *he, void *ht)
  * 参数: @ hash结构地址
  * 描述:
  *      1. 销毁hash的数据，但是hash结构本身不销毁
- *      2. 并且ht->data堆数组不销毁，但会清0
+ *      2. 并且ht->data堆数组不销毁，但会清0，hash其他回调钩子函数均保留
  **/
 void msd_hash_clear(msd_hash_t *ht)
 {
